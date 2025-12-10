fa1ea459283b7a86e5e38bf70fa7bb93df27250d
dammarin <marinatosd@gmail.com>
2025-12-10 20:54:08 +0200

    feat: add arrow functional component with pro

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import FunctionalComponent from "./components/FunctionalComponent.tsx";
 import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
 import ClassComponent from "./components/ClassComponent.tsx";
+import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWithProps.tsx";
 
 
 function App() {
+
+  const title = "Is an Arrow Functional Component!";
+
   return (
     <>
       <FunctionalComponent />
       <ArrowFunctionalComponent />
       <ClassComponent />
+      <ArrowFunctionalComponentWithProps title={title} />
     </>
   )
 }
