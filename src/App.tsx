ce4817ff30c6194fe774cca34edc11b2fc72727d
dammarin <marinatosd@gmail.com>
2025-12-15 18:34:07 +0200

    feat: add card component with title and children props

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import FunctionalComponent from "./components/FunctionalComponent.tsx";
 import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
 import ClassComponent from "./components/ClassComponent.tsx";
 import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWithProps.tsx";
-
+import Card from "./components/Card.tsx";
 
 function App() {
 
@@ -14,6 +14,14 @@ function App() {
       <ArrowFunctionalComponent />
       <ClassComponent />
       <ArrowFunctionalComponentWithProps title={title} />
+
+      <Card title={title}>
+        <div>
+          <h2>titleSecond</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum exercitationem
+            id qui quisquam quos repellendus tempore temporibus tenetur ullam voluptatem?</p>
+        </div>
+      </Card>
     </>
   )
 }
