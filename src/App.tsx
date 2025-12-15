6077b839e3bf2d5b1206fa5cf4732319ff35c496
dammarin <marinatosd@gmail.com>
2025-12-15 19:59:00 +0200

    feat: add Header, Footer, Layout components and use custom colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,39 @@
 import FunctionalComponent from "./components/FunctionalComponent.tsx";
-import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
-import ClassComponent from "./components/ClassComponent.tsx";
-import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWithProps.tsx";
-import Card from "./components/Card.tsx";
+// import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
+// import ClassComponent from "./components/ClassComponent.tsx";
+// import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWithProps.tsx";
+// import Card from "./components/Card.tsx";
+import Layout from "./components/layout/Layout.tsx";
 
 function App() {
 
-  const title = "Is an Arrow Functional Component!";
+  // const title = "Is an Arrow Functional Component!";
 
   return (
     <>
-      <FunctionalComponent />
-      <ArrowFunctionalComponent />
-      <ClassComponent />
-      <ArrowFunctionalComponentWithProps title={title} />
 
-      <Card title={title}>
-        <div>
-          <h2>titleSecond</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum exercitationem
-            id qui quisquam quos repellendus tempore temporibus tenetur ullam voluptatem?</p>
-        </div>
-      </Card>
+      <Layout>
+        {/* MAIN CONTENT*/}
+        <main className="container mx-auto p-8">
+          <h2>Main content</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, tempora!</p>
+          <FunctionalComponent />
+        </main>
+      </Layout>
+
+
+      {/*<FunctionalComponent />*/}
+      {/*<ArrowFunctionalComponent />*/}
+      {/*<ClassComponent />*/}
+      {/*<ArrowFunctionalComponentWithProps title={title} />*/}
+
+      {/*<Card title={title}>*/}
+      {/*  <div>*/}
+      {/*    <h2>titleSecond</h2>*/}
+      {/*    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum exercitationem*/}
+      {/*      id qui quisquam quos repellendus tempore temporibus tenetur ullam voluptatem?</p>*/}
+      {/*  </div>*/}
+      {/*</Card>*/}
     </>
   )
 }
