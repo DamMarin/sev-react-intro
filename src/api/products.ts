9c5f7a04558c2cc29df88a492cd79e01ee2f9761
dammarin <marinatosd@gmail.com>
2026-01-20 19:16:54 +0200

    feat(products): add schemas and api fetches

---
+++ src/api/products.ts
@@ -0,0 +1,41 @@
+import type {Product} from "@/schemas/products.ts";
+
+const API_URL = import.meta.env.VITE_API_URL;
+const TENANT_ID = import.meta.env.VITE_TENANT_ID;
+
+export async function getProducts(): Promise<Product[]> {
+  const res = await fetch(`${API_URL}/tenants/${TENANT_ID}/products/`,
+    {
+      method: "GET",
+      headers: {"Content-Type": "application/json"},
+    }
+    );
+  if (!res.ok) throw new Error("Failed to fetch products");
+  return await res.json();
+}
+
+export async function getProduct(id: number): Promise<Product> {
+  const res = await fetch(`${API_URL}/tenants/${TENANT_ID}/products/${id}`);
+  if (!res.ok) throw new Error("Failed to fetch products");
+  return await res.json();
+}
+
+export async function createProduct(data: Omit<Product, "id">) {
+  const res = await fetch(`${API_URL}/tenants/${TENANT_ID}/products/`,{
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify(data),
+  });
+  if (!res.ok) throw new Error("Failed to create product");
+  return await res.json();
+}
+
+export async function updateProduct(data: Partial<Product>, id: number): Promise<Product>{
+  const res = await fetch(`${API_URL}/tenants/${TENANT_ID}/products/${id}`, {
+    method: "PUT",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify(data),
+  });
+  if (!res.ok) throw new Error("Failed to update product");
+  return await res.json();
+}
